1e759e980ca42b75879d43097af9001311fdd6e1
Arden Wang <ardentwang@gmail.com>
2024-12-17 23:25:56 -0600

    init commit, set up Navbar and trying to set up routing

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return <div className="header">
+        <h1> Hello World </h1>
+        <button>Home</button>
+    </div>
+}
+
+export default Navbar
